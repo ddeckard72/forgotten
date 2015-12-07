de32ecd34fd002f42a17007e15833cadadeb6340
Don Deckard <don_deckard@yahoo.com>
2015-12-07 12:56:01 -0500

    framing out for future routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ post "/location", to: "location#create"
 patch "/location/:id", to: "location#edit"
 delete "/location/:id", to: "location#delete"
 get "/location", to: "location#index"
+#image
+
+#content
+
+#article
+
 
 end
