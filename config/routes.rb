61e44a7de4808e4dbbd2656832ce0056cde61692
Don Deckard <don_deckard@yahoo.com>
2016-02-18 23:53:17 -0500

    add routes for images

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ post "/location", to: "location#create"
 patch "/location/:id", to: "location#edit"
 delete "/location/:id", to: "location#delete"
 get "/location", to: "location#index"
+get "/location", to: "location#locations"
 #image
-
+post "/image", to: "image#add"
+get "/image/search", to: "image#search"
+delete "/image", to: "image#delete"
+get "/image", to: "image#imagelist"
 #content
 
 #article
